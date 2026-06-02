43f3db36fad578caf39a03ddc37842e2ccd0e623
vybzking <112782787+vybzking@users.noreply.github.com>
2026-06-02 19:10:29 +0000

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,2 +1,9 @@
-import {onAuthStateChanged } from ""
+import { 
+  getAuth, 
+  onAuthStateChanged, 
+  createUserWithEmailAndPassword, 
+  signInWithEmailAndPassword,
+  signOut 
+} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 import { auth } from "./config.js";
+
