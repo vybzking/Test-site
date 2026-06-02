5cff78dc1130779e7a5c4be46fd06eabf0f38163
vybzking <112782787+vybzking@users.noreply.github.com>
2026-06-02 19:20:18 +0000

    Update auth.js

--- auth.js
+++ auth.js
@@ -7,3 +7,14 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 import { auth } from "./config.js";
 
+onAuthStateChanged(auth, async (user) => {
+  if (user) {
+    console.log("Logged in user verified:", user.uid);
+  } else {
+    console.log("No token found. Booting back to login...");
+    // Only redirect if we aren't already on the login page to avoid loops
+    if (!window.location.pathname.includes("index.html")) {
+       window.location.href = "index.html";
+    }
+  }
+});
