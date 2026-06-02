57aed0d736d2ad2e6faf17dfcf082e97a5301b12
vybzking <112782787+vybzking@users.noreply.github.com>
2026-06-02 18:50:13 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,10 @@
 
+const firebaseConfig = {
+  apiKey: "AIzaSyCP8QucUl9sS_z5x7syJYJps8mAmocdjbo",
+  authDomain: "trial-project-33cbb.firebaseapp.com",
+  projectId: "trial-project-33cbb",
+  storageBucket: "trial-project-33cbb.firebasestorage.app",
+  messagingSenderId: "453484633595",
+  appId: "1:453484633595:web:2cc944c931387521fdff5e",
+  measurementId: "G-N9QJPYSGQM"
+};
