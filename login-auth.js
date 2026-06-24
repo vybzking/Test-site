f58bd10cfb2a722648ed27649e7dbbd72e410862
vybzking <112782787+vybzking@users.noreply.github.com>
2026-06-24 15:19:14 +0000

    Update login-auth.js

--- login-auth.js
+++ login-auth.js
@@ -3,19 +3,19 @@ import { auth, db } from "./config.js";
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 import { doc, getDoc } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
-onAuthStateChanged(auth, async (user) => {
-  if (user) {
-    const userSnap = await getDoc(doc(db, "users", user.uid));
-    if (userSnap.exists()) {
-      const role = userSnap.data().role;
-      // Redirect away from the login page to the proper dashboard
-      if (role === "teacher") window.location.href = "teachers-dashboard.html";
-      else if (role === "student") window.location.href = "students-dashboard.html";
-      else if (role === "admin") window.location.href = "admin-dashboard.html";
-    }
-  }
-  // If no user is logged in, we do nothing! We just let them look at the login page.
-});
+// onAuthStateChanged(auth, async (user) => {
+//   if (user) {
+//     const userSnap = await getDoc(doc(db, "users", user.uid));
+//     if (userSnap.exists()) {
+//       const role = userSnap.data().role;
+//       // Redirect away from the login page to the proper dashboard
+//       if (role === "teacher") window.location.href = "teachers-dashboard.html";
+//       else if (role === "student") window.location.href = "students-dashboard.html";
+//       else if (role === "admin") window.location.href = "admin-dashboard.html";
+//     }
+//   }
+//   // If no user is logged in, we do nothing! We just let them look at the login page.
+// });
 
 
 const login = async function(e) {
