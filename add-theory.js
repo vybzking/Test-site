ece1713ed6d33eef3a3f0cf180cfb610bf49ef2e
vybzking <112782787+vybzking@users.noreply.github.com>
2026-07-02 22:42:10 +0000

    Update add-theory.js

--- add-theory.js
+++ add-theory.js
@@ -137,7 +137,7 @@ onAuthStateChanged(auth, async (user) => {
 
         formData.append("file", file);
         formData.append("upload_preset", cloudinaryConfig.uploadPreset);
-        formData.append("folder", "assignments");
+        formData.append("folder", "documents/assignments");
 
         const response = await fetch(
           cloudinaryConfig.assignmentURI,
